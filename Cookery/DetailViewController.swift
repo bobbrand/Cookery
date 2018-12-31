f0a6196b96e13c82b1d16d3511fe8f34f41ee6ed
Bob Brand <bobbrand@bobbrand.com>
2018-12-31 13:56:25 -0800

    Camera is working without image store

--- Cookery/DetailViewController.swift
+++ Cookery/DetailViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
-class DetailViewController: UIViewController, UITextFieldDelegate {
+class DetailViewController: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
+    // MARK: - Objects
     @IBOutlet var dishNameField: UITextField!
     @IBOutlet var categoryNameField: UITextField!
     @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var recipeImageView: UIImageView!
     
+    // MARK: - Actions
     @IBAction func backgroundTapped(_ sender: UITapGestureRecognizer) {
         view.endEditing(true)
     }
@@ -24,13 +27,31 @@ class DetailViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
+    @IBAction func takePicture(_ sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+        
+        // If the device has a camera, take a picture
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+        } else {
+            imagePicker.sourceType = .photoLibrary
+        }
+        
+        imagePicker.delegate = self
+        
+        // Place image picker on the screen
+        present(imagePicker, animated: true, completion: nil)
+    }
+  
+    // MARK: - UIViewController Functions and Util
+
     let dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .medium
         formatter.timeStyle = .none
         return formatter
     } ()
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -50,9 +71,22 @@ class DetailViewController: UIViewController, UITextFieldDelegate {
         item.dishCategory = categoryNameField.text ?? ""
     }
     
+    // MARK: - Text Field Delegate Functions
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
     
+    // MARK: - UIImagePickerControllerDelegate Functions
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // Get a picked image from the info dictionary
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        
+        // Put the image on the screen in the image view
+        recipeImageView.image = image
+        
+        // Remove the image picker
+        dismiss(animated: true, completion: nil)
+    }
 }
